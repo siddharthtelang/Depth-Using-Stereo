fd8f97c845322fb6437fbfd7fb95557cfb38c614
Siddharth Telang <stelang@umd.edu>
2021-06-16 12:35:39 -0400

    add function to get the fundamental matrix

--- stereo.py
+++ stereo.py
@@ -28,3 +28,39 @@ def normalizePoints(pts):
     x = np.column_stack((pts, np.ones(len(pts))))
     x_norm = (T.dot(x.T)).T
     return x_norm, T
+
+def getFundamentalMatrix(src, dst):
+    col = 9
+    # need minimum of 8 points to compute F Matrix
+    if src.shape[0] > 7:
+        #do normalization
+        src_norm, T1 = normalizePoints(src)
+        dst_norm, T2 = normalizePoints(dst)
+        A = np.zeros((len(src_norm),9))
+
+        for i in range(len(src_norm)):
+            x1, y1 = src_norm[i][0], src_norm[i][1]
+            x2, y2 = dst_norm[i][0], dst_norm[i][1]
+            A[i] = np.array([x1*x2, x2*y1, x2, y2*x1, y2*y1, y2, x1, y1, 1])
+
+        #calculate SVD of A
+        U, S, VT = np.linalg.svd(A, full_matrices=True)
+        #F = VT.T[:, -1]
+        #F = F.reshape(3,3)
+        F = VT[-1,:]
+        F = F.reshape(3,3)
+
+        #the calculated F matrix can be of rank 3, but to find epipolar lines, the rank should be 2.
+        #thus, 
+        U_, S_, VT_ = np.linalg.svd(F)
+        # make diagonal matrix and set last element to 0
+        S_ = np.diag(S_)
+        S_[2,2] = 0
+        #recompute F
+        F = np.dot(U_, np.dot(S_, VT_))
+
+        #un-normalize
+        F = np.dot(T2.T, np.dot(F, T1))
+        return F
+    else:
+        return None
